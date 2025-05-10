b8de16f09aba0c106d8dc219ddcf277aefbf5972
Jackie-PC <jackiekuo.sir@gmail.com>
2025-05-10 14:03:43 +0800

    IntelliJ reset

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,4 @@
-module com.example.test {
+module com.app.emolist {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
@@ -11,6 +11,6 @@ module com.example.test {
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
 
-    opens app to javafx.fxml;
-    exports app;
+    opens com.app.emolist to javafx.fxml;
+    exports com.app.emolist;
 }
